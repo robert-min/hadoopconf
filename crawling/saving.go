406e93e9fd0b50bb83e409d2699e3dcf0efe71cf
robert-min <robertmin522@gmail.com>
2023-01-07 22:49:44 +0900

    mongo db data create error -ing

--- crawling/saving.go
+++ crawling/saving.go
@@ -19,6 +19,53 @@ import (
 var dbName string = "config"
 var colName string = "hdfs"
 
+type config struct {
+	Name        string
+	Value       string `bson:"value,omitempty"`
+	Description string `bson:"description,omitempty"`
+}
+
+func Parsing(path string) {
+	data, err := ioutil.ReadFile(path)
+	check(err)
+	docs := strings.Split(string(data), "\n\n")
+	allDatas := []interface{}{}
+	var name string = ""
+	var value string = ""
+	var description string = ""
+	var b bytes.Buffer
+	for _, context := range docs {
+		lines := strings.Split(context, "\n")
+
+		for _, line := range lines {
+			line = strings.TrimLeft(line, " ")
+			if strings.HasPrefix(line, "<name>") {
+				name = strip.StripTags(line)
+			} else if strings.HasPrefix(line, "<value>") {
+				value = strip.StripTags(line)
+			} else if strings.HasPrefix(line, "</property>") {
+				description = b.String()
+				description = strip.StripTags(description)
+				allDatas = append(allDatas, config{Name: name, Value: value, Description: description})
+				b.Reset()
+			} else {
+				b.WriteString(line)
+			}
+		}
+	}
+	client, ctx, cancel := connectionMongo()
+	defer client.Disconnect(ctx)
+	defer cancel()
+
+	coll := client.Database(dbName).Collection(colName)
+	result, err := coll.InsertMany(context.TODO(), allDatas)
+	check(err)
+	fmt.Printf("%d documents inserted with IDs:\n", len(result.InsertedIDs))
+	for _, id := range result.InsertedIDs {
+		fmt.Printf("\t%s\n", id)
+	}
+}
+
 // connectionMongo to connect DB
 func connectionMongo() (client *mongo.Client, ctx context.Context, cancel context.CancelFunc) {
 
@@ -44,64 +91,6 @@ func connectionMongo() (client *mongo.Client, ctx context.Context, cancel contex
 	return client, ctx, cancel
 }
 
-// GetCollection to connect collection
-func GetCollection(client *mongo.Client, colName string) *mongo.Collection {
-	return client.Database(dbName).Collection(colName)
-}
-
-func createConfig(name string, value string, description string) {
-	// client, ctx, cancel := connectionMongo()
-	// defer client.Disconnect(ctx)
-	// defer cancel()
-
-	// num, err := GetCollection(client, colName).CountDocuments(ctx, bson.M{"name": name})
-	// check(err)
-
-	// GetCollection(client, colName).InsertOne()
-
-	return nil
-}
-
-type config struct {
-	Name        string
-	Value       string
-	Description string
-}
-
-func Parsing(path string) {
-	data, err := ioutil.ReadFile(path)
-	check(err)
-	docs := strings.Split(string(data), "\n\n")
-
-	for _, context := range docs {
-		lines := strings.Split(context, "\n")
-		var b bytes.Buffer
-		configMap := new(config)
-
-		for _, line := range lines {
-			line = strings.TrimLeft(line, " ")
-			if strings.HasPrefix(line, "<name>") {
-				name := strip.StripTags(line)
-				configMap.Name = name
-			} else if strings.HasPrefix(line, "<value>") {
-				value := strip.StripTags(line)
-				configMap.Value = value
-			} else if strings.HasPrefix(line, "</property>") {
-				description := b.String()
-				description = strip.StripTags(description)
-				configMap.Description = description
-				createConfig(configMap.Name, configMap.Value, configMap.Description)
-
-				configMap = new(config)
-				b.Reset()
-			} else {
-				b.WriteString(line)
-			}
-		}
-
-	}
-
-}
 func check(e error) {
 	if e != nil {
 		fmt.Println(e)
